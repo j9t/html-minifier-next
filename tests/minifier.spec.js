bad687646d7a51c94a403310189753fa6d647ccb
Jens Oliver Meiert <jens@meiert.com>
2025-10-16 17:36:19 +0200

    fix: correct typo in minifier test case
    Signed-off-by: Jens Oliver Meiert <jens@meiert.com>

--- tests/minifier.spec.js
+++ tests/minifier.spec.js
@@ -2,7 +2,7 @@ import { test } from 'node:test';
 import assert from 'node:assert';
 import { minify } from '../src/htmlminifier.js';
 
-test('`minifiy` exists', () => {
+test('`minify` exists', () => {
   assert.ok(minify);
 });
 
