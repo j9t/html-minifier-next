7ce8455a6c52015a4c3b46b9ad2a7a66145fd607
Jens Oliver Meiert <jens@meiert.com>
2026-01-05 14:57:00 +0100

    refactor: remove unused index.js
    Signed-off-by: Jens Oliver Meiert <jens@meiert.com>

--- src/lib/index.js
+++
@@ -1,7 +0,0 @@
-export * from './utils.js';
-export * from './constants.js';
-export * from './elements.js';
-export * from './attributes.js';
-export * from './content.js';
-export * from './whitespace.js';
-export * from './options.js';
