796e15973e18d9dfb2d343053a7ab1911e63938f
Jens Oliver Meiert <jens@meiert.com>
2026-02-02 11:36:02 +0100

    chore: resolve CodeQL alert
    https://github.com/j9t/html-minifier-next/security/code-scanning/4
    Signed-off-by: Jens Oliver Meiert <jens@meiert.com>

--- src/lib/whitespace.js
+++ src/lib/whitespace.js
@@ -62,13 +62,31 @@ function collapseWhitespace(str, options, trimLeft, trimRight, collapseAll) {
   }
 
   if (options.preserveLineBreaks) {
-    str = str.replace(/^[ \n\r\t\f]*?[\n\r][ \n\r\t\f]*/, function () {
-      lineBreakBefore = '\n';
-      return '';
-    }).replace(/[ \n\r\t\f]*?[\n\r][ \n\r\t\f]*$/, function () {
-      lineBreakAfter = '\n';
-      return '';
-    });
+    // Find leading/trailing whitespace containing line breaks manually
+    // (avoids polynomial backtracking with end-anchored lazy quantifiers)
+    const WS_CHARS = ' \n\r\t\f';
+    let leadEnd = 0;
+    while (leadEnd < str.length && WS_CHARS.includes(str[leadEnd])) {
+      leadEnd++;
+    }
+    if (leadEnd > 0) {
+      const leading = str.slice(0, leadEnd);
+      if (/[\n\r]/.test(leading)) {
+        lineBreakBefore = '\n';
+        str = str.slice(leadEnd);
+      }
+    }
+    let trailStart = str.length;
+    while (trailStart > 0 && WS_CHARS.includes(str[trailStart - 1])) {
+      trailStart--;
+    }
+    if (trailStart < str.length) {
+      const trailing = str.slice(trailStart);
+      if (/[\n\r]/.test(trailing)) {
+        lineBreakAfter = '\n';
+        str = str.slice(0, trailStart);
+      }
+    }
   }
 
   if (trimLeft) {
